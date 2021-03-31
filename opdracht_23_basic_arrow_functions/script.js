c391ed1bc958e2fe7c2293657b2ae37cfa4376a9
sjpoot <78816393+sjpoot@users.noreply.github.com>
2021-03-31 13:18:30 +0200

    Add files via upload

---
+++ opdracht_23_basic_arrow_functions/script.js
@@ -0,0 +1,23 @@
+// zet de functie om naar een arrow functie
+let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+  
+ikRockArrowFunctions()
+
+// functie 5 + 7
+let fivePlusSeven = () => 5 + 7;
+  
+console.log(fivePlusSeven());
+
+// functie a + b
+let myFunction = (a, b) => a + b;
+
+console.log(myFunction(21, 10));
+
+// functie addFive
+let addFive = a => a + 5;
+console.log(addFive(11));
+
+// functie createObject
+let createObject = () => ({greeting: "hoi"});
+
+console.log(createObject());
